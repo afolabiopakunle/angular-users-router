50bb19387a1d2a9f82d8aba01af0b3210ab69102
afolabiopakunle <opakunleafolabi@gmail.com>
2023-04-11 23:58:49 +0100

    all artifacts imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { FormsModule } from '@angular/forms';
+import { ServersService } from './servers/servers.service';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
+    FormsModule,
   ],
-  declarations: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    ServerComponent,
+    UsersComponent,
+    EditServerComponent,
+  ],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
