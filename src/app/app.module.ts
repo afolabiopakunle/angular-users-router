294c633ee5bcea85ce882355deac1a12dd0e6d10
afolabiopakunle <opakunleafolabi@gmail.com>
2023-04-12 23:23:13 +0100

    app routing module exported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,22 +11,8 @@ import { FormsModule } from '@angular/forms';
 import { ServersService } from './servers/servers.service';
 import { UserComponent } from './users/user/user.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent, 
-  children: [
-  {path: ':id/:name', component: UserComponent},
-  ]},
-  {path: 'servers', component: ServersComponent, 
-  children: [
-    {path: ':id', component: ServerComponent},
-    {path: ':id/edit', component: EditServerComponent},]},
-  {path: 'not-found', component: PageNotFoundComponent},  
-  {path: '**', redirectTo: 'not-found', pathMatch: 'full'},
-]
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
@@ -34,7 +20,7 @@ const routes: Routes = [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes),
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
