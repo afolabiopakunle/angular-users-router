787591f3a5d37c4117f2e9e6c8c605c2902a4ba5
afolabiopakunle <opakunleafolabi@gmail.com>
2023-04-12 17:58:59 +0100

    user route children implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'users/:id/:name', component: UserComponent},
+  {path: 'users', component: UsersComponent, 
+  children: [
+  {path: ':id/:name', component: UserComponent},
+  ]},
   {path: 'servers', component: ServersComponent, 
   children: [
     {path: ':id', component: ServerComponent},
